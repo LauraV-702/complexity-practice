4f081fc5981302bac2560325bd20ff3573fdee97
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2024-10-22 21:49:25 -0700

    Answered computeAverage

--- src/Main.java
+++ src/Main.java
@@ -132,10 +132,11 @@ public class Main {
   }
 
   // The time complexity is:
-  // YOUR ANSWER HERE
+  // O(1)
   public static double computeAverage(double a, double b) {
     return (a + b) / 2.0;
   }
+
   // The time complexity is:
   // YOUR ANSWER HERE
   public static void checkIfContainedHashSet(HashSet<String> set, String target)
